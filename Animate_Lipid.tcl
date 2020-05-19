8a23effdf11604ae16dce9dce5fd1bce47ff990a
Bassam Haddad <bhaddad@pdx.edu>
2020-05-18 19:28:08 -0700

    Added Comments

--- Animate_Lipid.tcl
+++ Animate_Lipid.tcl
@@ -1,14 +1,25 @@
 # This is a module of LipNetwork.tcl, and is used to visualize lipid-frames only from lipids that were classified as being
-# within a density.
+# within a density. It takes a list of lipids (of interest) as an input. LipList is a list full of specific lipid's attributes,
+# created during the lip_analysis() proc of LipNetwork.tcl. 
+#
+# Run lipid_animator() after running lip_analysis, then use the literal string 'LipList' as your input.
+#
+# Inputs: 
+#	- LipList:	ResID	SegID	LipID-1	LipID-2	frame	num-Carbons-1	num-Carbons-2
+#	  Index 	  0	  1	   2	   3	  4	      5		     6
+#
+#	- MolID
 
 proc lipid_animator {LipList molid} {
 
 	set numframes [molinfo $molid get numframes]
-
+	
+	# Initialize and fill lists of SegIDs & ResIDs from LipList.
+	
 	set	SegList	""
 	
 	set	ResList ""
-
+	
 	foreach lipid $LipList {
 
 		set	hold 	[list [lindex $lipid 0] [lindex $lipid 1]]
@@ -34,6 +45,8 @@ proc lipid_animator {LipList molid} {
 		}	
 	}
 	
+	# Create list of frames where the lipid is within the density. 
+	
 	foreach resid $ResList segid $SegList {
 		
 		set	frame_list	""
@@ -53,6 +66,8 @@ proc lipid_animator {LipList molid} {
 			}
 
 		}
+	
+		# Load full .dcd, chop it up using the previously asscertained frame_list, and create a .dcd for each lipid.
 	
 		puts [llength $frame_list]
 
